b3244619dccdafa0d7dd0458275911e41444866f
eric-ramos-garnica <ericramoshackbright@gmail.com>
2023-03-02 18:56:31 -0800

    Final project

--- calculator.py
+++ calculator.py
@@ -6,6 +6,7 @@ from arithmetic import (add, subtract, multiply, divide, square, cube,
 
 # Replace this with your code
 def validation():
+
     user_input = input("Enter input:")
     tokens = user_input.split()
     
@@ -19,6 +20,8 @@ def validation():
 
     
     if len(tokens)<3:
+
+        
         while True:
                 if tokens[0] != '+' and tokens[0] != '-' and tokens[0] != '*' and tokens[0] != '/' and tokens[0] != 'square' and tokens[0] != 'cube' and tokens[0] != 'pow' and tokens[0] != 'mod' and tokens[0] != 'q':
                     print("Please enter proper command (+, -, *, /, square, cube, pow, mod )")
@@ -26,6 +29,8 @@ def validation():
                     tokens = user_input.split()
                 if tokens[0] == 'q':
                         return tokens
+        
+
                 if not tokens[1].isdigit():
                     print("Please use a number")
                     user_input = input("Re-enter input:")
@@ -51,12 +56,20 @@ def validation():
     
     return tokens
 
-
+#main program
 while True:
-
-    tokens= validation()
-    # user_input = input("Enter input:")
-    # tokens = user_input.split()
+    file_answer= input("Would you like to get your inputs with file['YES' or 'NO']:")
+    if file_answer == 'q':
+        print("exit")
+        break
+    if file_answer.upper() == "YES" or file_answer.upper() == "Y":
+        file_txt = open("answer.txt")
+        for line in file_txt:
+            tokens =line.split()
+            break          
+    else:
+        tokens= validation()
+       
     
     if 'q' in tokens:
         print("exit")
@@ -71,27 +84,27 @@ while True:
 
     if operator == "+":
         result = add(num1,num2)
-        print(result)
+        print(f'{result:.2f}')
     elif operator == "-":
         result = subtract(num1, num2)
-        print(result)
+        print(f'{result:.2f}')
     elif operator == "*":
         result = multiply(num1, num2)
-        print(result)
+        print(f'{result:.2f}')
     elif operator == "/":
         result = divide(num1, num2)
-        print(result)
+        print(f'{result:.2f}')
     elif operator == "square":
         result = square(num1)
-        print(result)
+        print(f'{result:.2f}')
     elif operator == "cube":
         result = cube(num1)
-        print(result)
+        print(f'{result:.2f}')
     elif operator == "pow":
         result = power(num1, num2)
-        print(result)
+        print(f'{result:.2f}')
     elif operator == "mod":
         result = mod(num1, num2)
-        print(result)
+        print(f'{result:.2f}')
 
 
